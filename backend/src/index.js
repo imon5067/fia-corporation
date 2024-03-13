8939f8ac658e732b831f2a301a3d90505943aa63
or-imon <orimon9@gmail.com>
2024-03-14 01:51:00 +0200

    cloudinary use, backend index and frontend code update

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,10 +32,3 @@ app.use("/api/orders", orderRoutes);
 app.listen(port, () => {
     console.log("listening on port http://localhost:" + port);
 });
-
-//const __dirname = path.resolve();
-//app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
-
-//app.get("/api/config/paypal", (req, res) => {
- //   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
- // });
