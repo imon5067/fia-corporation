03d466ece2faad98cb9819efc54069757061b7ca
or-imon <orimon9@gmail.com>
2024-03-14 01:51:00 +0200

    cloudinary use, backend index and frontend code update

--- backend/src/cloudinary.js
+++ backend/src/cloudinary.js
@@ -1,9 +1,9 @@
 import {v2 as cloudinary} from 'cloudinary';
           
-const cloudinary.config({ 
+const cloudinaryService= cloudinary.config({ 
   cloud_name: Process.env.CLOUDINARY_NAME, 
   api_key: Process.env.CLOUDINARY_API, 
   api_secret: Process.env.CLOUDINARY_SECERET_KEY, 
 });
 
-module.exports=cloudinary;
+module.exports=cloudinaryService;
